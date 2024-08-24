5b97e2a9d252ac25dc6e2f3a33dd9801689401db
Islom_the_dev <polzovatelit1@gmail.com>
2024-08-24 18:13:01 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-let students = ['johongir',"alan","safina","laylo","azim","farxod","alisher","jasur","islom"]
+let students = ['johongir',"alan","safina","laylo","azim","farhod","alisher","jasur","islom"]
 let username = prompt("Введите имя которое хотите удалить!").trim().toLowerCase()
 let index = students.indexOf(username)
 
 if (students.includes(username)) {
     students.splice(index,1)
+    alert("Все прошло успешно!")
+
 } else {
     alert("Сорри но такого имени нету в списке")
 }
